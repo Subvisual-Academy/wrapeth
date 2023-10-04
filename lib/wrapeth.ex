27d382edf08ee2d555504ad7b9dce18c21eca697
Wander Souza <56940846+SouzaWander@users.noreply.github.com>
2023-10-04 14:12:02 +0100

    Initialise Elixir Project (#5)
    This pull request aims to initialise the Elixir project within the
    repository.

---
+++ lib/wrapeth.ex
@@ -0,0 +1,18 @@
+defmodule Wrapeth do
+  @moduledoc """
+  Documentation for `Wrapeth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wrapeth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wrapeth_test.exs
@@ -0,0 +1,8 @@
+defmodule WrapethTest do
+  use ExUnit.Case
+  doctest Wrapeth
+
+  test "greets the world" do
+    assert Wrapeth.hello() == :world
+  end
+end
