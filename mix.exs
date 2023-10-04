6f75f1d10073f9709293fa3b9c8ad74f0c75f7bd
SouzaWander <wanderpereirasouza@gmail.com>
2023-10-04 11:03:57 +0100

    Removes unnecessary application file.

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Wrapeth.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Wrapeth.Application, []}
+
     ]
   end
 
