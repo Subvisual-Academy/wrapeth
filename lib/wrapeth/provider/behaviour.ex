7e0a0a3f2c248b7decb52cf6a79a1ad626a17c8e
SouzaWander <wanderpereirasouza@gmail.com>
2023-10-10 15:59:31 +0100

    Refactor file structure and introduce new fetch methods in Provider Interface module

---
+++ lib/wrapeth/provider/behaviour.ex
@@ -0,0 +1,14 @@
+defmodule Wrapeth.Provider.Behaviour do
+  @type error :: {:error, map() | binary() | atom()}
+  @type address :: String.t()
+  @type hex_value :: String.t()
+
+  # API methods
+  @callback get_module_and_url() :: map() | error()
+  @callback get_accounts() :: any()
+  @callback get_block_number() :: hex_value()
+  @callback get_balance(address, String.t()) :: hex_value()
+  @callback get_block_by_number(hex_value(), boolean()) :: map()
+  @callback get_transaction_count(address(), hex_value()) :: hex_value()
+  @callback get_block_transaction_count_by_number(hex_value()) :: hex_value()
+end
