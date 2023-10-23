1a35ffa48a23dc7aadf32ed4166a9b0f5654dd0c
SouzaWander <wanderpereirasouza@gmail.com>
2023-10-23 16:19:02 +0100

    Refactor call client to provide more flexible error handling and update test provider tests to use setup functions instead of module attributes.

--- lib/wrapeth/provider.ex
+++ lib/wrapeth/provider.ex
@@ -12,59 +12,48 @@ defmodule Wrapeth.Provider do
 
       @impl true
       def eth_accounts(_args \\ []) do
-        {:ok, accounts} = call_client(:eth_accounts)
-        accounts
+        call_client(:eth_accounts)
       end
 
       @impl true
       def eth_get_balance(address, block \\ "latest", _args \\ []) do
-        {:ok, balance} = call_client(:eth_get_balance, [address, block])
-        balance
+        call_client(:eth_get_balance, [address, block])
       end
 
       @impl true
       def eth_block_number(_args \\ []) do
-        {:ok, block_number} = call_client(:eth_block_number)
-        block_number
+        call_client(:eth_block_number)
       end
 
       @impl true
       def eth_get_block_by_number(number, full, _args \\ []) do
-        {:ok, block} = call_client(:eth_get_block_by_number, [number, full])
-        block
+        call_client(:eth_get_block_by_number, [number, full])
       end
 
       @impl true
       def eth_get_transaction_count(address, block \\ "latest", _args \\ []) do
-        {:ok, tx_count} = call_client(:eth_get_transaction_count, [address, block])
-        tx_count
+        call_client(:eth_get_transaction_count, [address, block])
       end
 
       @impl true
       def eth_get_block_transaction_count_by_number(block \\ "latest", _args \\ []) do
-        {:ok, tx_count} = call_client(:eth_get_block_transaction_count_by_number, [block])
-
-        tx_count
+        call_client(:eth_get_block_transaction_count_by_number, [block])
       end
 
       @impl true
       def eth_gas_price(_args \\ []) do
-        {:ok, gas} = call_client(:eth_gas_price)
-
-        gas
+        call_client(:eth_gas_price)
       end
 
       @impl true
       def eth_get_transaction_receipt(hash, _args \\ []) do
-        {:ok, receipt} = call_client(:eth_get_transaction_receipt, [hash])
-
-        receipt
+        call_client(:eth_get_transaction_receipt, [hash])
       end
 
       defp call_client(method_name, args \\ [], _arg \\ []) do
         case apply(@client_type, method_name, args ++ [[{:url, @node_url}]]) do
           {:ok, value} -> {:ok, value}
-          {:error, error} -> raise(error)
+          {:error, error} -> {:error, error}
         end
       end
     end
--- test/wrapeth/provider_test.exs
+++ test/wrapeth/provider_test.exs
@@ -2,6 +2,18 @@ defmodule Wrapeth.ProviderTest do
   use ExUnit.Case, async: true
   import Mox
 
+  setup do
+    {:ok, valid_address: "0x4Bd040d48fdD1C667fcE5fdDd326681766E6ad91"}
+  end
+
+  setup do
+    {:ok, invalid_address: "0y4Bd040d48fdD1C667fcE5fdDd326681766E6ad91"}
+  end
+
+  setup do
+    {:ok, invalid_block_number: "0y9710d"}
+  end
+
   defmodule TestProvider do
     use Wrapeth.Provider, otp_app: :wrapeth
   end
@@ -9,101 +21,83 @@ defmodule Wrapeth.ProviderTest do
   # Make sure mocks are verified when the test exits
   setup :verify_on_exit!
 
-  @valid_address "0x4Bd040d48fdD1C667fcE5fdDd326681766E6ad91"
-  @invalid_addres "0y4Bd040d48fdD1C667fcE5fdDd326681766E6ad91"
-  @invalid_block_number "0y9710d"
-
-  test "gets accounts" do
+  test "gets accounts", %{valid_address: valid_address} do
     HttpMock
-    |> expect(:eth_accounts, fn _ -> {:ok, [@valid_address]} end)
+    |> expect(:eth_accounts, fn _ -> {:ok, [valid_address]} end)
 
-    assert TestProvider.eth_accounts() ==
-             [@valid_address]
+    assert TestProvider.eth_accounts() == {:ok, [valid_address]}
   end
 
-  test "gets account balance" do
+  test "gets account balance", %{valid_address: valid_address} do
     HttpMock
     |> expect(:eth_get_balance, fn _some_addr, _, _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_get_balance(@valid_address) ==
-             "0x123"
+    assert TestProvider.eth_get_balance(valid_address) == {:ok, "0x123"}
   end
 
   test "gets block by number" do
     HttpMock
     |> expect(:eth_get_block_by_number, fn _some_block_number, _, _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_get_block_by_number("some_block_number", true) ==
-             "0x123"
+    assert TestProvider.eth_get_block_by_number("some_block_number", true) == {:ok, "0x123"}
   end
 
   test "gets transaction count" do
     HttpMock
     |> expect(:eth_get_transaction_count, fn _some_addr, _, _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_get_transaction_count("some_address", "latest") ==
-             "0x123"
+    assert TestProvider.eth_get_transaction_count("some_address", "latest") == {:ok, "0x123"}
   end
 
   test "gets transaction count by number" do
     HttpMock
     |> expect(:eth_get_block_transaction_count_by_number, fn _block, _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_get_block_transaction_count_by_number("latest") ==
-             "0x123"
+    assert TestProvider.eth_get_block_transaction_count_by_number("latest") == {:ok, "0x123"}
   end
 
   test "gets block number" do
     HttpMock
     |> expect(:eth_block_number, fn _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_block_number() ==
-             "0x123"
+    assert TestProvider.eth_block_number() == {:ok, "0x123"}
   end
 
   test "gets gas price" do
     HttpMock
     |> expect(:eth_gas_price, fn _ -> {:ok, "0x123"} end)
 
-    assert TestProvider.eth_gas_price() ==
-             "0x123"
+    assert TestProvider.eth_gas_price() == {:ok, "0x123"}
   end
 
   test "gets transaction receipt" do
     HttpMock
     |> expect(:eth_get_transaction_receipt, fn _tx, _ -> {:ok, {"0x123"}} end)
 
-    assert TestProvider.eth_get_transaction_receipt("tx_hash") ==
-             {"0x123"}
+    assert TestProvider.eth_get_transaction_receipt("tx_hash") == {:ok, {"0x123"}}
   end
 
-  test "error get balance invalid address" do
+  test "error get balance invalid address", %{invalid_address: invalid_address} do
     HttpMock
     |> expect(:eth_get_balance, fn _invalid_addr, _, _ ->
-      {:error, "invalid 1st argument: address value was not valid hexadecimal"}
+      {:error, "invalid 1st argument: address value was not a valid hexadecimal"}
     end)
 
-    assert_raise(
-      RuntimeError,
-      "invalid 1st argument: address value was not valid hexadecimal",
-      fn ->
-        TestProvider.eth_get_balance(@invalid_address)
-      end
-    )
+    assert TestProvider.eth_get_balance(invalid_address) ==
+             {:error, "invalid 1st argument: address value was not a valid hexadecimal"}
   end
 
-  test "error get block by number with invalid block number" do
+  test "error get block by number with an invalid block number", %{
+    invalid_block_number: invalid_block_number
+  } do
     HttpMock
     |> expect(:eth_get_block_by_number, fn _invalid_block_num, _, _ ->
-      {:error, "invalid 1st argument: block_number value was not valid block tag or block number"}
+      {:error,
+       "invalid 1st argument: block_number value was not a valid block tag or block number"}
     end)
 
-    assert_raise(
-      RuntimeError,
-      "invalid 1st argument: block_number value was not valid block tag or block number",
-      fn ->
-        TestProvider.eth_get_block_by_number(@invalid_block_number, true)
-      end
-    )
+    assert TestProvider.eth_get_block_by_number(invalid_block_number, true) ==
+             {:error,
+              "invalid 1st argument: block_number value was not a valid block tag or block number"}
   end
 end
