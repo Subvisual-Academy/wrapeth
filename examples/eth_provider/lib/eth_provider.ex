430fc99aa1cf7b11a0d7ed5d29b7aa3be84dc15f
SouzaWander <wanderpereirasouza@gmail.com>
2023-10-25 14:17:06 +0100

    formats the code

--- examples/eth_provider/lib/eth_provider.ex
+++ examples/eth_provider/lib/eth_provider.ex
@@ -2,7 +2,7 @@ defmodule EthProvider do
   use Wrapeth.Provider, otp_app: :wrapeth
 
   def get_latest_block_number do
-   {:ok, block_number} = eth_block_number()
-   block_number
+    {:ok, block_number} = eth_block_number()
+    block_number
   end
 end
