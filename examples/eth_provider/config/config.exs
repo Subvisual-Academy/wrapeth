8b54bf4c67eda6ea741920aa390275175cdce01d
SouzaWander <wanderpereirasouza@gmail.com>
2023-10-25 14:14:11 +0100

    Remove the Polygon provider as it was somewhat trivial.

--- examples/eth_provider/config/config.exs
+++ examples/eth_provider/config/config.exs
@@ -3,3 +3,7 @@ import Config
 config :wrapeth, EthProvider,
   client_type: Ethereumex.HttpClient,
   node_url: System.get_env("ETH_NODE_URL")
+
+# config :wrapeth, PolygonProvider,
+#   client_type: Ethereumex.HttpClient,
+#   node_url: System.get_env("POLY_NODE_URL")
